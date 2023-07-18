fd3b5cc4ab8fa54c15d5538e636d0e000f49310d
Aa.y <a.a.y.king.chess@gmail.com>
2023-07-19 00:16:56 +0330

    create form

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import UserRegisterationForm
+class RegisterView(View):
+    def get(self, request):
+        form = UserRegisterationForm()
+        return render(request, 'account/register.html', {'form':form})
 
-# Create your views here.
+    def post(self, request):
+        pass
