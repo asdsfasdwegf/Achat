36644d145d2c6bb73d82748346ba3f3eb4322187
Aa.y <a.a.y.king.chess@gmail.com>
2023-07-25 17:13:59 +0330

    edit forms

--- account/views.py
+++ account/views.py
@@ -8,10 +8,11 @@ from django.contrib import messages
 class RegisterView(View):
     form_class = UserRegisterationForm
     template_name = 'account/register.html'
+
     def get(self, request):
         form = self.form_class()
         form = UserRegisterationForm()
-        return render(request, self.template_name, {'form':form})
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
         form = self.form_class(request.POST)
